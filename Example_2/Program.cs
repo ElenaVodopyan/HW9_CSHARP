597070b883315362af7a35143a0ec066d0e29e3e
Elena <vodopyan.elena@gmail.com>
2022-10-03 23:42:22 +0300

    Example_2

---
+++ Example_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SummMtoN(int m, int n)
+{
+    if (m == n) return n;
+    return m + SummMtoN(m + 1, n);
+}
+
+Console.WriteLine("Задайте значения M и N (M < N):");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SummMtoN(m, n));
