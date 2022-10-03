86be4044ca57eaf7abedf02cb4b1b3b63798cd6d
Elena <vodopyan.elena@gmail.com>
2022-10-03 23:27:22 +0300

    Example_1

---
+++ Example_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+void RecursionN(int n)
+{
+    Console.Write($"{n} ");
+    if (n == 1) return;
+    RecursionN(n - 1);
+}
+
+Console.WriteLine("Задайте значение N:");
+int n = Convert.ToInt32(Console.ReadLine());
+RecursionN(n);
